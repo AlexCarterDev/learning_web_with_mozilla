622dde71a0029b571f028222a40688d02ec24649
AlexCarterDev <AlexCarterDeveloper@gmail.com>
2018-05-29 02:13:56 +0300

    replace h1 text to 'Hello World'

---
+++ JavaScript/p01 - Hello World/scripts/main.js
@@ -0,0 +1,4 @@
+/* The Document method querySelector() returns the first Element within
+the document that matches the specified selector, or group of selectors.*/
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World!';
