7c79e4d797c74ff1a996b68fa82a9bc04be55aa5
AlexCarterDev <AlexCarterDeveloper@gmail.com>
2018-05-29 02:22:15 +0300

    change image by click

--- JavaScript/p01 - Hello World/scripts/main.js
+++ JavaScript/p01 - Hello World/scripts/main.js
@@ -1,4 +1,18 @@
+/* replace h1 text */
 /* The Document method querySelector() returns the first Element within
 the document that matches the specified selector, or group of selectors.*/
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello World!';
+
+/* change image by click */
+var myImage = document.querySelector('img');
+myImage.onclick = function() {
+  var firefoxIconPath = 'images/firefox-icon.png';
+  var cityImgPath = 'images/city.jpg';
+  var imgSrc = myImage.getAttribute('src');
+  if (imgSrc === firefoxIconPath) {
+    myImage.setAttribute('src', cityImgPath);
+  } else {
+    myImage.setAttribute('src', firefoxIconPath);
+  }
+}
