5ee7192121d20ea302de232d0a7d8b61ab70bd75
AlexCarterDev <AlexCarterDeveloper@gmail.com>
2018-06-12 15:08:11 +0300

    Create new images and add them to thumb bar

--- JavaScript/p05 - Image gallery/main.js
+++ JavaScript/p05 - Image gallery/main.js
@@ -5,10 +5,12 @@ btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-
+for (var i = 0; i < 5; i++) {
+  var imgPath = 'images/pic' + (i + 1) + '.jpg';
   var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
+  newImage.setAttribute('src', imgPath);
   thumbBar.appendChild(newImage);
+}
+
 
 /* Wiring up the Darken/Lighten button */
-
